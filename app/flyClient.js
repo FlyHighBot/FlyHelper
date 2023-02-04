2ba592d4827d8196ab75aebb7485b0077936a356
BRENOsx <brenokelvin9@gmail.com>
2023-02-04 10:23:33 -0300

    Update flyClient.js

--- app/flyClient.js
+++ app/flyClient.js
@@ -6,7 +6,7 @@ module.exports = class flyClient{
   constructor(client) {
     this.client = client
 
-    client.support = client.guilds.cache.get("696919133551591496")
+    client.support = client.guilds.cache.get("")
     client.guild = client.guilds.cache.get("696919133551591496")
     client.owner = client.users.cache.get("557355064864538624")
 
