68bd8dd9c819438a74626edfbb0195c74b5097bb
BRENOsx <brenokelvin9@gmail.com>
2023-02-23 00:54:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ client.constants = Constants
 require('discord-logs')(client);
 
 const fly = new flyClient(client)
-  fly.loadEvents()
+ fly.loadEvents()
  fly.loadCommands()
  fly.loadSlashs()
  fly.loadSubCommands()
