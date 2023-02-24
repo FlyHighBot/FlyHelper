24b80f8371884822ad416eff00035db560381af5
BRENOsx <brenokelvin9@gmail.com>
2023-02-24 17:46:07 -0300

    Update HowToGetCoinsResponse.js

--- app/responses/support/HowToGetCoinsResponse.js
+++ app/responses/support/HowToGetCoinsResponse.js
@@ -18,7 +18,7 @@ module.exports = class HowToGetCoinsResponse extends ServerResponse {
   
   async run(client, message) {
     message.build(
-      message.flyReply("você pode ganhar **Fly Coins**.... Dormindo! Ksksks brincadeirinha! Existe uma maneira muito simples de ganhar **Fly Coins**, apenas coletando daily!", "826430254084128829"),
+      message.flyReply("você pode ganhar **Fly Coins**.... Dormindo! Ksksk brincadeirinha! Existe uma maneira muito simples de ganhar **Fly Coins**, apenas coletando daily!", "826430254084128829"),
       message.flyReply("Também há como apostar **Fly Coins** com um amigo, apostar é uma boa maneira de ganhar (ou perder se você for azarado) coins de forma fácil e divertida!", "826431152671817728", false),
       message.flyReply("Mas não se preocupe! Se você não gosta de apostas, você pode gastar seus coins comprando backgrounds novos ou até mesmo layouts diferentes para o seu perfil", "826415473775869962", false)
     )
