4598cc9eb683ee3fe4a641fe4ba75ea03ef9a80a
BRENOsx <brenokelvin9@gmail.com>
2023-04-05 07:46:37 -0300

    Update ServersIpResponse.js

--- app/responses/community/ServersIpResponse.js
+++ app/responses/community/ServersIpResponse.js
@@ -22,7 +22,7 @@ module.exports = class ServersIPResponse extends ServerResponse {
   
   async run(client, message) {
     message.build(
-      message.flyReply(`vamos por partes... A Rede FlyHigh apresenta uma váriedade de servidores. Para descobrir o ip de algum servidor, você pode ir no <#826349439245221918> e ir no tópico do canal, igual é mostrado nessa imagem. E digitar o comando do servidor desejado!`, "826431055161983026"),
+      message.flyReply(`Vamos por partes... A Rede FlyHigh apresenta uma váriedade de servidores. Para descobrir o ip de algum servidor, você pode ir no <#> e ir no tópico do canal, igual é mostrado nessa imagem. E digitar o comando do servidor desejado!`, "826431055161983026"),
       message.flyReply(`Não entendeu? Vamos tentar assim, vá no <#${client.constants.FLY_SUPPORT_CHANNEL_ID}> e mencione os <@&${client.util.FLY_SUPPORT_ID}> para que eles possam te ajudar a responder essa pergunta! Uma boa tarde!`, "826430254084128829", false),
       `{ "option": { "allowedMentions": { "roles": [""], "users": ["${message.author.id}"] } } }`
     )
