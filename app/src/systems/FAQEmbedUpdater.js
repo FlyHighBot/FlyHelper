7aed5139928d0ee290a2dd556c2a13164c099bf8
BRENOsx <brenokelvin9@gmail.com>
2023-05-13 09:38:19 -0300

    Update FAQEmbedUpdater.js

--- app/src/systems/FAQEmbedUpdater.js
+++ app/src/systems/FAQEmbedUpdater.js
@@ -3,8 +3,7 @@ const { stackFAQ } = require('../utils/messageUtils')
 module.exports = {
   run: async (client, message) => {
      if (message.channel.type !== "dm") {
-       if (message.author == client.user) return
-       stackFAQ(message, "Perguntas Frequentes", "769900882887180288")
+       if (message.author == client.user) return stackFAQ(message, "Perguntas Frequentes", "769900882887180288")
      }
   },
   
