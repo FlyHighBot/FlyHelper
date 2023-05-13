c47cf447517fb32e6527b8a9a316f7bcffc63e50
BRENOsx <brenokelvin9@gmail.com>
2023-05-13 09:36:49 -0300

    Update GoodDaySystem.js

--- app/src/systems/GoodDaySystem.js
+++ app/src/systems/GoodDaySystem.js
@@ -7,8 +7,7 @@ module.exports = {
     const job = new CronJob('0 */24 * * *', async () => {
       var channel = client.channels.cache.get("")
       
-      channel.build(
-        `${checkEmoji(client, "826414452969963520")} **|** Um outro dia nasce, staff do fly, que o hoje seja melhor que o ontem!`
+      channel.build(`${checkEmoji(client, "826414452969963520")} **|** Um outro dia nasce, staff do fly, que o hoje seja melhor que o ontem!`
       )
     }, null, true, 'America/Sao_Paulo')
     
